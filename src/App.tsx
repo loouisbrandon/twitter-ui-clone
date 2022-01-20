7ee947e64f07d99ed9b7c9d2b45e29ff9e7d9450
loouisbrandon <brandonlouisferreira@gmail.com>
2022-01-20 02:17:32 -0300

    saved 1

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import GlobalStyles from "./styles/GlobalStyles";
+function App() {
+    return (
+        <>
+        <h1>Brandon</h1>
+    <GlobalStyles />
+    </>
+    );
+
+}
+
+export default App;
